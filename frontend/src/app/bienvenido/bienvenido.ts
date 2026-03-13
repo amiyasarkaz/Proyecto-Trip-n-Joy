16f733dbcfef295adfe1a6c4b7014d35b33718cf
ruben1421 <rubenantoniomunoz0@gmail.com>
2026-03-13 17:04:34 +0100

    Creacion del componente bienvenido

---
+++ frontend/src/app/bienvenido/bienvenido.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bienvenido',
+  imports: [],
+  templateUrl: './bienvenido.html',
+  styleUrl: './bienvenido.css',
+})
+export class Bienvenido {}
