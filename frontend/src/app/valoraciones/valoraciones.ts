9c94ca21e55b9ccb7a9d6655e706a22ef9d78afd
amiyasarkaz <deividsonic2@gmail.com>
2026-03-13 16:24:44 +0100

    Commit creacion del componente valoraciones

---
+++ frontend/src/app/valoraciones/valoraciones.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-valoraciones',
+  imports: [],
+  templateUrl: './valoraciones.html',
+  styleUrl: './valoraciones.css',
+})
+export class Valoraciones {}
