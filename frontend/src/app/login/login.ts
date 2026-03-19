1edcf232b32dc445334499294ab161c5355c09be
ruben1421 <rubenantoniomunoz0@gmail.com>
2026-03-19 16:13:22 +0100

    Popups
    Union de los popups en la pagina que-ofrecemos

--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
   templateUrl: './login.html',
-  styleUrls: ['./login.css'] 
+  styleUrls: ['./login.css']
 })
 export class Login {}
