1c7a4449b38e449fac4e4a3c13883153965a5521
ruben1421 <rubenantoniomunoz0@gmail.com>
2026-03-20 16:49:00 +0100

    Conexion al popup de bienvenido

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Registro } from './registro/registro';
 import { Login } from './login/login';
-import { Header } from './shared/header/header'; 
+import { Header } from './shared/header/header';
+import { Bienvenido } from './bienvenido/bienvenido'; 
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,8 @@ import { Header } from './shared/header/header';
     RouterOutlet, 
     Registro, 
     Login,
-    Header 
+    Header,
+    Bienvenido 
   ],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
