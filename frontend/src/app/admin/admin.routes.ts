9c828035ec2c50b03d40af9dec02a8bbbcf6899f
amiyasarkaz <deividsonic2@gmail.com>
2026-03-25 15:26:21 +0100

    Pruebas

--- frontend/src/app/admin/admin.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { Dashboard } from './dashboard';
-import { adminGuard } from './admin.guard';
-
-
-export const adminRoutes: Routes = [
-  { path: 'admin/dashboard', component: Dashboard, canActivate: [adminGuard] }
-];
